05b175910552af7a0b2750de9fddd5bf12ed4e1f
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-03-20 18:22:13 -0300

    feat: created page for profile

---
+++ src/components/buttons/TextButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { createEventDispatcher, type EventDispatcher } from "svelte";
+
+    const dispatch: EventDispatcher<MouseEvent> = createEventDispatcher();
+    
+</script>
+
+<button on:click={(event: MouseEvent) => dispatch('click', event)}>
+
+</button>
