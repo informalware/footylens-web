0751677ea825a220ec0efd6ba64cc090f6132e8e
Thiago Duvanel <thiago.duvanel.ferreira@live.com>
2024-03-28 18:27:37 -0300

    Base para os tipos

---
+++ src/lib/data/types.ts
@@ -0,0 +1,39 @@
+
+interface Team {
+    id: number;
+
+    name: string;
+}
+
+interface Match {
+    id: number;
+    duration: number;
+
+    team1: Team;
+    team2: Team;
+
+    score1: number;
+    score2: number;
+
+    date: Date;
+    location: string;
+}
+
+interface Tournament {
+    matches: Match[];
+
+    teams: Team[];
+    places: Team[];
+}
+
+
+interface User {
+    id: number;
+
+    name: string;
+    at: string;
+    email: string;
+    
+    team: Team;
+    friends: string[];
+}
