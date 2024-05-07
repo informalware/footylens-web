5b75b43d90c6821acaca02049ab7ac2d46b37ffe
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-05-07 16:59:30 -0300

    Uses new author and commentary display

--- src/routes/reviews/[id]/+page.svelte
+++ src/routes/reviews/[id]/+page.svelte
@@ -1,61 +1,31 @@
 <script lang="ts">
-	import type { Review, User, Commentary } from '$lib/data/types';
-	import { backend_address } from '$lib/consts';
 	import Subtitle from '$lib/components/texts/subtitle.svelte';
 	import { page } from '$app/stores';
-	import axios from 'axios';
 	import RatingDisplay from '$components/review/rating-display.svelte';
-	import { stringToDate } from '$lib/str_utils';
 	import AuthorDisplay from '$components/review/author-display.svelte';
+	import CommentaryCard from '$components/review/commentary-card.svelte';
+	import { req_review, req_user, req_commentaries_from_review } from '$lib/requests';
 
-    let id = $page.params.id
-
-    async function review_req(): Promise<Review> {
-        const res = await axios.get(backend_address + `/reviews/${id}`);
-
-        return {...res.data}
-    }
-    // Solicita o autor ao backend
-    async function author_req (id: any): Promise<User> {
-        const res = await axios.get(backend_address + `/users/${id}`);
-
-        return {...res.data}
-    }
-
-    async function all_commentaries_req() {
-        const res = await axios.get(backend_address + `/reviews/${id}/commentaries`);
-
-        return {...res.data}
-    }
-
-    async function commentary_req(id: any): Promise<Commentary> {
-        const res = await axios.get(backend_address + `/commentaries/${id}`);
-
-        return {...res.data}
-    }
+    let id = Number($page.params.id)
 </script>
 
-{#await review_req() then review}
-    {#await author_req(review.userId) then author}
+{#await req_review(id) then review}
     <p>Partida: {review.matchId}</p>
     
     <div class="review-container">
-        <AuthorDisplay author={author} date={review.creationDate} />
+        <AuthorDisplay id={review.userId} date={review.creationDate} />
         <p>{review.review}</p>
         <RatingDisplay rating={review.rating} />
     </div>
     
     <Subtitle left>Comentários</Subtitle>
     <div class="comments-container">
-        {#await all_commentaries_req() then comment_list}
+        {#await req_commentaries_from_review(review.id) then comment_list}
         {#each comment_list.commentaries as cid}
-        {#await commentary_req(cid) then commentary}
-        <p>{commentary.id} - {commentary.commentary}</p>
-        {/await}
+        <CommentaryCard id={cid} />
         {/each}
         {/await}
     </div>
-    {/await}
 {/await}
 
 <style>
@@ -70,5 +40,8 @@
     }
     .comments-container {
         margin: 2rem;
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
     }
 </style>
