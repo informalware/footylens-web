23bbe9c6fe954ed29d85094eb7af8a369490c948
OJarrisonn <j.h.m.t.v.10@gmail.com>
2024-05-07 16:58:57 -0300

    Fetches the user from the display

--- src/lib/components/review/author-display.svelte
+++ src/lib/components/review/author-display.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
-	import type { User } from "$lib/data/types";
+	import { goto } from "$app/navigation";
+	import { req_user } from "$lib/requests";
 	import { stringToDate } from "$lib/str_utils";
 	import Subtitle from "../texts/subtitle.svelte";
     import { User as UserIcon } from "lucide-svelte";
 
-    export let author: User;
+    export let id: number;
     export let date: string | undefined;
     $: formatted_date = stringToDate(date);
+
+    function navigateToUserProfile(username: string) {
+        goto(`/users/@${username}`);
+    }
 </script>
 
-<div>
-    
-    <Subtitle left><UserIcon class="inline"/> {author.display}</Subtitle>
-    {#if formatted_date}
-    <p>{formatted_date}</p>
-    {/if}
-</div>
+{#await req_user(id) then user}
+<div role="button" tabindex="0" aria-label="View user profile" on:click={() => navigateToUserProfile(user.username)} on:keydown={() => navigateToUserProfile(user.username)}>
+    <Subtitle left><UserIcon class="inline"/> {user.display}</Subtitle>
+        {#if formatted_date}
+        <p>{formatted_date}</p>
+        {/if}
+    </div>
+{/await}
 
